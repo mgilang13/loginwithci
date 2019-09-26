be7b547a1d640a6bb89fd5c7dd0fcc6349fd57a5
mgilang13 <muhammad.gilang.n.k@gmail.com>
2019-09-26 18:26:14 +0700

    Pemisahan Redirect Admin Page dan User Page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,17 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class User extends CI_Controller 
+class Admin extends CI_Controller 
 {
     public function index()
     {
-        $data['title_page'] = "My Profile";
+        $data['title_page'] = "Dashboard";
         $data['users'] = $this->db->get_where('users', ['email' => $this->session->userdata('email')])->row_array();
         
         $this->load->view('templates/dashboard/header', $data);
         $this->load->view('templates/dashboard/sidebar', $data);
         $this->load->view('templates/dashboard/topbar', $data);
-        $this->load->view('user/index', $data);
+        $this->load->view('admin/index', $data);
         $this->load->view('templates/dashboard/footer');
     }
 }
